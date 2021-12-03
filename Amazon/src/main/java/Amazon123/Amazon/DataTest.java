26ec1d126d71f00c0c8c8e3d39ee5abaf1be8fbf
Pankaj-Winner <Pankaj-Winner@DESKTOP-2E44PGS>
2021-12-03 16:01:10 +0530

    Hello, upload all

---
+++ Amazon/src/main/java/Amazon123/Amazon/DataTest.java
@@ -0,0 +1,24 @@
+package Amazon123.Amazon;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class DataTest {
+	
+	@Test
+	public  void Data()
+	{
+	System.setProperty("webdriver.chrome.driver", "C:\\Manisha\\drivers\\45\\chromedriver.exe");
+	
+	WebDriver driver=new ChromeDriver();
+	
+	driver.get("https://amazon.in");
+	
+	LoginTest login1=new LoginTest( driver);
+	
+	login1.login().click();
+
+	}
+
+}
---
+++ Amazon/src/main/java/Amazon123/Amazon/LoginTest.java
@@ -0,0 +1,31 @@
+package Amazon123.Amazon;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+
+
+public class LoginTest {
+	
+	WebDriver Driver;
+	
+
+	
+	public LoginTest(WebDriver driver) {
+		this.Driver=driver;
+	}
+		
+	
+
+	By login=By.className("nav-action-inner");
+
+	public WebElement login() {
+		
+		return Driver.findElement(login);
+	}
+	
+	
+	
+
+}
